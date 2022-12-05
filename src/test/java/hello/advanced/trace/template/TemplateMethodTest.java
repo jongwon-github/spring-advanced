ca493c68650af67a6da330c3295ab50bdbcdc736
jwlee-9939-study <jwlee9939@gmail.com>
2022-12-05 21:37:54 +0900

    3. 템플릿 메서드 패턴과 콜백 패턴 > 템플릿 메서드 패턴1 - 예제2

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,9 +1,10 @@
 package hello.advanced.trace.template;
 
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-
-import java.awt.desktop.ScreenSleepEvent;
 
 @Slf4j
 public class TemplateMethodTest {
@@ -30,6 +31,18 @@ public class TemplateMethodTest {
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
         log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
     }
 
 }

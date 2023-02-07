b1221d343429e5c1600cb452d0c1d1c1fbbfafa9
jwlee-9939-study <jwlee9939@gmail.com>
2023-02-07 22:53:14 +0900

    3. 템플릿 메서드 패턴과 콜백 패턴 > 템플릿 콜백 패턴 - 적용

--- src/main/java/hello/advanced/app/v5/OrderServiceV5.java
+++ src/main/java/hello/advanced/app/v5/OrderServiceV5.java
@@ -1,31 +1,28 @@
 package hello.advanced.app.v5;
 
+import hello.advanced.trace.callback.TraceTemplate;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.template.AbstractTemplate;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
- *  1. AbstractTemplate<Void>
- *   - 제네릭에서 반환 타입이 필요한데, 반환할 내용이 없으면 Void 타입을 사용하고 null 을 반환하면 된다.
- *     참고로제네릭은기본타입인void,int 등을선언할수없다.
  */
 @Service
-@RequiredArgsConstructor
 public class OrderServiceV5 {
 
     private final OrderRepositoryV5 orderRepository;
-    private final LogTrace trace;
+    private final TraceTemplate template;
+
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace trace) {
+        this.orderRepository = orderRepository;
+        this.template = new TraceTemplate(trace);
+    }
 
     public void orderItem(String itemId) {
-        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
-            @Override
-            protected Void call() {
-                orderRepository.save(itemId);
-                return null;
-            }
-        };
-        template.execute("OrderService.orderItem()");
+        template.execute("OrderService.orderItem()", () -> {
+            orderRepository.save(itemId);
+            return null;
+        });
     }
 
 }
